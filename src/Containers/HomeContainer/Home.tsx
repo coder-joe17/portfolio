209ed2100cd6b2ff2cd809cfaa2a2603581fb65e
Joseph Bingham <jabbusiness4231@gmail.com>
2018-10-30 22:25:00 -0700

    finished home content and added responsive styling

--- src/Containers/HomeContainer/Home.tsx
+++ src/Containers/HomeContainer/Home.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import {hot} from "react-hot-loader";
 import './Home.scss'
+import { Welcome } from '../../Components/HomeComponents/WelcomeComponent/Welcome'
 
 class Home extends React.Component{
     render() {
         return (
             <div className='homeContainer'>
-                <h1></h1>
+                <Welcome />
             </div>
         )
     }
