2687204698b279f54de495fb0ede810fec6365e9
Joe Bingham <joe@iwaandr.com>
2020-03-09 19:24:06 -0700

    Fix typo

--- src/Components/ContactComponents/Contact.tsx
+++ src/Components/ContactComponents/Contact.tsx
@@ -6,7 +6,7 @@ export class ContactMain extends React.Component {
     render() { 
         return (
             <div className='contactContainer'>
-                <h1>Want to reach out? I'd love to here from you!</h1>
+                <h1>Want to reach out? I'd love to hear from you!</h1>
                 <p>Just shoot me an email at Joe@Coder-Joe.com</p>
 
             </div>
